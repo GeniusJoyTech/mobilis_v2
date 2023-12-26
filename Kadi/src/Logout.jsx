4431d35020976dfdcb725869fc3b3c85de6eab13
geniusjoytech <geniusjoytech@gmail.com>
2023-12-25 23:52:46 -0300

    Versão 1 do componente de Logout.

--- Kadi/src/Logout.jsx
+++ Kadi/src/Logout.jsx
@@ -1,3 +1,20 @@
-function deleteCookie(cookieName) {
-    document.cookie = `${cookieName}=; path=/;`;
-}
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+const Logout = () => {
+    function deleteCookie(cookieName) {
+        document.cookie = `${cookieName}=; path=/;`;
+    }
+
+    const handleLogoutClick = () => {
+        deleteCookie('Authorization');
+    };
+
+    return (
+        <Button variant="primary" type="submit" onClick={handleLogoutClick}>
+            Logout
+        </Button>
+    );
+};
+
+export default Logout;
