fc2009656e0bfdf480a09eb073a7d71b689c2f5f
geniusjoytech <geniusjoytech@gmail.com>
2024-01-27 09:46:07 -0300

    27/01 9:45 Atualização do componente Modal, agora value não recebe mais null.

--- Kadi/src/components/Modal.jsx
+++ Kadi/src/components/Modal.jsx
@@ -1,55 +1,51 @@
 import { Modal, Button, Form, Dropdown, DropdownItem } from 'react-bootstrap';
-import React, { useState } from 'react';
-
-export default function CustomModal({ titulo, show, onHide, col, list, form, handleInputChange, handleSendSubmit, handleDeletSubmit, handleDropdownSelect }) {
-    const [selectedItem, setSelectedItem] = useState(null);
-    console.log(form);
+import React from 'react';
+export default function CustomModal({ titulo, show, onHide, col, form, list, handleInputChange, handleSendSubmit, handleDeletSubmit }) {
+    //{ titulo === 'Incluir' ? console.log('incluir', form) : console.log('editar', form) }
     return (
         <Modal show={show} onHide={onHide}>
             <Modal.Header closeButton>
                 <Modal.Title>{titulo}</Modal.Title>
             </Modal.Header>
-            {list != null ?   (
-                <Dropdown onSelect={() => {}}>
-                    <Dropdown.Toggle variant="secondary">
-                        {selectedItem ? `${Object.values(selectedItem).join(', ')}` : 'Escolha'}
-                    </Dropdown.Toggle>
-                    <Dropdown.Menu>
-                        {list.map((item, index) => (
-                            <DropdownItem key={index} onClick={() => { 
-                                setSelectedItem(item);
-                                handleDropdownSelect(item);
-                            }}>
-                                {Object.keys(item).map((key) => (
-                                    `${item[key]}`
-                                )).join(' - ')}
-                            </DropdownItem>
-                        ))}
-                    </Dropdown.Menu>
-                </Dropdown>
-            ) : null}
+            {/* Inicio dropDown */}
+            {list != null && <Dropdown>
+                <Dropdown.Toggle variant="secondary">
+                    Selecione para {titulo}
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                    {list.map((item, index) => (
+                        <DropdownItem key={index}>{Object.values(item).join(' - ')}</DropdownItem>
+                    )
+                    )}
+                </Dropdown.Menu>
+            </Dropdown>}
+            {/*Fim DropDown */}
             <Modal.Body>
-                <Form>
-                    {col.map((coluna) => (
-                        coluna.tipo === 'form' && (
-                            <Form.Group key={coluna.id} controlId={`form${coluna.nome}`}>
-                                <Form.Label>{coluna.nome}</Form.Label>
-                                <Form.Control type="text" name={coluna.nome} onChange={handleInputChange} />
-                            </Form.Group>
-                        )
+                {form != null 
+                &&
+                (
+                    <Form>
+                    {Object.entries(form).map(([key, value], index) => (
+                        <Form.Group key={index} controlId={`form${key}`}>
+                            <Form.Label>{key}</Form.Label>
+                            <Form.Control type="text" name={key} value={value === null ? '' : value} onChange={handleInputChange} />
+                        </Form.Group>
                     ))}
                 </Form>
+                )
+                }
             </Modal.Body>
             <Modal.Footer>
                 <Button variant="primary" onClick={handleSendSubmit}>
                     Salvar Alterações
                 </Button>
                 {handleDeletSubmit != null ?
+
                     <Button variant="danger" onClick={handleDeletSubmit}>
                         Deletar
                     </Button>
                     :
-                    null
+                    <></>
                 }
             </Modal.Footer>
         </Modal>
